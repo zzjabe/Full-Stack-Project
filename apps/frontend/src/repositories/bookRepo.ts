fda13ccdac62993bdfddfdcc590c8b1f4ddfcd6d
Jianbin Zhang <jianbinzhang@rrc.ca>
2026-04-04 02:48:36 -0500

    Updated again 9.

--- apps/frontend/src/repositories/bookRepo.ts
+++ apps/frontend/src/repositories/bookRepo.ts
@@ -1,7 +1,6 @@
 import type { Book } from "../../../../shared/types/book";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const bookRepo = {
   async findAll(): Promise<Book[]> {
